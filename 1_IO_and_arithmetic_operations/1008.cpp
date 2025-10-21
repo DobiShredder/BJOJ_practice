a9d30cef807e95ae2119b4d45e2bf688f526019f
DobiShredder <luire12@gmail.com>
2025-10-21 17:40:40 +0900

    20251021

---
+++ 1_IO_and_arithmetic_operations/1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	double a, b, c;
+	std::cin >> a >> b;
+	if ( a < 0 && a > 10 && b < 0 && b > 10 )
+		std::cout << "error!" << std::endl;
+	else
+		c = a / b;
+		std::cout << std::setprecision(20) << c << std::endl;
+	return 0;
+}
